bf1c4eda60747902437a45596ec1cee009399ea5
Dipesh Biswa <db4048@g.rit.edu>
2025-11-01 22:57:44 -0400

    task module completed

--- src/main/java/model/viewModel/service/taskService.java
+++ src/main/java/model/viewModel/service/taskService.java
@@ -1,8 +1,11 @@
 package model.viewModel.service;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Component;
 
 import model.persistence.TaskFileDAO;
+import model.persistence.task;
 
 @Component
 public class taskService {
@@ -10,5 +13,25 @@ public class taskService {
     
     public taskService(TaskFileDAO taskDAO){
         this.taskDAO = taskDAO;
+    }
+
+    public task[] getTasks() throws IOException{
+        return taskDAO.getTasks();
+    }
+    public task getTask(int id) throws IOException{
+        return taskDAO.getTask(id);
+    }
+    public task[] findTasks(String name) throws IOException{
+        return taskDAO.findTasks(name);
+    }
+    public task addTask(task task) throws IOException{
+        return taskDAO.createTask(task);
+        
+    }
+    public boolean deleteTask(int id) throws IOException{
+        return taskDAO.deleteTask(id);
+    }
+    public task updateTask(task task) throws IOException{
+        return taskDAO.updateTask(task);
     }
 }
