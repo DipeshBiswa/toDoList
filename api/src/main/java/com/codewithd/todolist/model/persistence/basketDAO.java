49d4dd3efafa7bed0b593f2ee5176b183f4510d6
Dipesh Biswa <db4048@g.rit.edu>
2025-11-23 21:19:11 -0500

    basketFileDAO implemented

--- api/src/main/java/com/codewithd/todolist/model/persistence/basketDAO.java
+++ api/src/main/java/com/codewithd/todolist/model/persistence/basketDAO.java
@@ -1,5 +1,7 @@
 package com.codewithd.todolist.model.persistence;
 
+import java.io.IOException;
+
 import com.codewithd.todolist.model.basket;
 import com.codewithd.todolist.model.task;
 
@@ -10,46 +12,46 @@ public interface basketDAO {
      * @param userId id of the certain user 
      * @return basket of the certain user
      */
-    basket getBasket(int userId);
+    basket getBasket(int userId)  throws IOException;
 
     /**
      * creates a new basket for user
      * @param userId if of the user
      * @return created basket
      */
-    basket createBasket(int userId);
+    basket createBasket(int userId) throws IOException;
 
     /**
      * updates a users basket
      * @param userId id of the user
      * @return updated basket
      */
-    basket updateBasket(int userId);
+    basket updateBasket(basket basket) throws IOException;
 
     /**
      * deletes the users basket
      * @param userId id of the user
      * @return true if deleted, false otherwise
      */
-    boolean deleteBasket(int userId);
+    boolean deleteBasket(int userId)throws IOException;
 
     /**
      * adds a task to basket
      * @param task task being aded
      * @return true if added, false otherwise
      */
-    boolean addToBasket(task task);
+    boolean addToBasket(int userId,task task)throws IOException;
 
     /**
      * removes a task from basket
      * @param taskId id of the task
      * @return true if removed, false otherwise
      */
-    boolean removeFromBasket(int taskId);
+    boolean removeFromBasket(int userId, int taskId) throws IOException;
 
     /**
      * clears all task from basket
      * @param userId id of the user
      */
-    void clearBasket(int userId);
+    void clearBasket(int userId) throws IOException;
 }
